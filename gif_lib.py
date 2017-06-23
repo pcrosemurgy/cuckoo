81d6351791b22f6afa838fea5c0cb361cf3ae7df
Paul Rosemurgy <pcrosemurgy@gmail.com>
2017-06-22 21:22:48 -0500

    test

--- gif_lib.py
+++ gif_lib.py
@@ -41,7 +41,6 @@ def resizeGif(path):
     p = im.getpalette()
     last_frame = im.convert('RGBA')
     allFrames = []
-    i = 0
     try:
         while True:
             if not im.getpalette():
@@ -52,12 +51,12 @@ def resizeGif(path):
             new_frame.paste(im, (0, 0), im.convert('RGBA'))
             new_frame.thumbnail((imW, imH), Image.ANTIALIAS)
             allFrames.append(new_frame)
-            i += 1
             last_frame = new_frame
             im.seek(im.tell()+1)
-    except EOFError:
-        pass
+    except:
+        return False
     allFrames[0].save(path, optimize=True, save_all=True, append_images=allFrames[1:], loop=1000)
+    return True
 
 def downloadGifs():
     os.system('rm -f data/img/day/*')
@@ -65,17 +64,21 @@ def downloadGifs():
     cSecret = '713c722f9ad5d145682067e405ece58b67a66b93'
     client = ImgurClient(cId, cSecret)  
     gal = client.subreddit_gallery('catgifs', sort='best', window='day')
-    for e in [e.link for e in gal if e.link[-3:] == 'gif']:
+#for e in [e.link for e in gal if e.link[-3:] == 'gif']:
+    count = 0
+    for e in ['http://i.imgur.com/i23JJ4f.gif']:
+        if count == 6:
+            break
         byteSize = int(urllib.urlopen(e).info().getheaders("Content-Length")[0])
         if byteSize > 1024**2*3 or byteSize == 0:
             continue
         os.system("wget -P data/img/day {} 2>/dev/null".format(e))
         path = 'data/img/day'+e[e.rfind('/'):]
-        if isGifAnimated(path):
-            print(e)
-            resizeGif(path)
+        if isGifAnimated(path) and resizeGif(path):
+            count += 1 
         else:
             try:
                 os.remove(path)
             except OSError:
                 pass
+downloadGifs()
