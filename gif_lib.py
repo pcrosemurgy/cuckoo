c6b8f5f57914bdad109ff0389343670b32386488
Paul Rosemurgy <pcrosemurgy@gmail.com>
2017-06-22 19:42:16 -0500

    made gif lib

---
+++ gif_lib.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import urllib
+from PIL import Image
+from imgurpython import ImgurClient
+
+def isGifAnimated(gif):
+    try:
+        frame = Image.open(gif)
+        frame.seek(1)
+        frame.seek(2)
+        return True
+    except:
+        return False
+
+def analyzeImage(path):
+    im = Image.open(path)
+    results = {'size': im.size, 'mode': 'full'}
+    try:
+        while True:
+            if im.tile:
+                tile = im.tile[0]
+                update_region = tile[1]
+                update_region_dimensions = update_region[2:]
+                if update_region_dimensions != im.size:
+                    results['mode'] = 'partial'
+                    break
+            im.seek(im.tell()+1)
+    except EOFError:
+        pass
+    return results
+
+def resizeGif(path, resizeTo=(480, 320)):
+    mode = analyzeImage(path)['mode']
+    im = Image.open(path)
+    imW, imH = im.size
+    x, y = (0, 0)
+
+    if imW > 480:
+        resizeTo[0] = 480
+    elif imW < 480:
+        x = 480/2-imW/2
+    if imH > 480:
+        resizeTo[1] = 320
+    elif imH < 320:
+        y = 320/2-imH/2
+    if imW < 480 and imH < 320:
+        return x, y
+
+    p = im.getpalette()
+    last_frame = im.convert('RGBA')
+    allFrames = []
+    i = 0
+    try:
+        while True:
+            if not im.getpalette():
+                im.putpalette(p)
+            new_frame = Image.new('RGBA', im.size)
+            if mode == 'partial':
+                new_frame.paste(last_frame)
+            new_frame.paste(im, (0, 0), im.convert('RGBA'))
+            new_frame.thumbnail(resizeTo, Image.ANTIALIAS)
+            allFrames.append(new_frame)
+            i += 1
+            last_frame = new_frame
+            im.seek(im.tell()+1)
+    except EOFError:
+        pass
+    allFrames[0].save(path, optimize=True, save_all=True, append_images=allFrames[1:], loop=1000)
+    return x, y
+
+def downloadGifs():
+    cId = '672625cda895fbb'
+    cSecret = '713c722f9ad5d145682067e405ece58b67a66b93'
+    client = ImgurClient(cId, cSecret)  
+
+    gal = client.subreddit_gallery('catgifs', sort='best', window='day')
+    for e in [e.link for e in gal if e.link[-3:] == 'gif']:
+        byteSize = int(urllib.urlopen(e).info().getheaders("Content-Length")[0])
+        if byteSize > 1024**2*3 or byteSize == 0:
+            continue
+        os.system("wget -P data/img/day {} 2>/dev/null".format(e))
+        path = 'data/img/day'+e[e.rfind('/'):]
+        if isGifAnimated(path):
+            print(e)
+        else:
+            try:
+                os.remove(path)
+            except OSError:
+                pass
--- gif_manager.py
+++ gif_manager.py
@@ -1,50 +1,23 @@
 #!/usr/bin/env python
 import os
-import urllib
-import subprocess
-from PIL import Image
-from imgurpython import ImgurClient
+import sys
+from gif_lib import *
 import pyglet
 
-def isGifAnimated(inGif):
-    try:
-        frame = Image.open(inGif)
-        frame.seek(1)
-        frame.seek(2)
-        return True
-    except:
-        return False
-
 if len(sys.argv) > 1:
-    cId = '672625cda895fbb'
-    cSecret = '713c722f9ad5d145682067e405ece58b67a66b93'
-    client = ImgurClient(cId, cSecret)  
+    downloadGifs()
 
-    gal = client.subreddit_gallery('catgifs', sort='best', window='day')
-    for e in [e.link for e in gal if e.link[-3:] == 'gif']:
-        byteSize = int(urllib.urlopen(e).info().getheaders("Content-Length")[0])
-        if byteSize > 1024**2*3 or byteSize == 0:
-            continue
-        os.system("wget -P data/img/day {} 2>/dev/null".format(e))
-        i = e.rfind('/')
-        path = 'data/img/day'+e[i:]
-        animated = isGifAnimated(path)
-        if animated:
-            print(e)
-        else:
-            try:
-                os.remove(path)
-            except OSError:
-                pass
-
-window = pyglet.window.Window(450, 450)
+window = pyglet.window.Window(480, 320)
 #window = pyglet.window.Window(fullscreen=True)
 
-a = pyglet.image.load_animation('data/img/day/C0yuLiK.gif')
-s = pyglet.sprite.Sprite(a)
+gif = '2AMqHUJ.gif'
+x1, y1 = resizeGif('data/img/day/'+gif, (480, 320))
+a = pyglet.image.load_animation('data/img/day/'+gif)
+s = pyglet.sprite.Sprite(a, x=x1, y=y1)
 
 @window.event
 def on_draw():
+    window.clear()
     s.draw()
 
 pyglet.app.run()
