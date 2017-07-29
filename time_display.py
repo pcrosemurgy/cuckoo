9b28f9c5e153de2717dfbd1073ba50af2f27d199
Paul Rosemurgy <pcrosemurgy@gmail.com>
2017-07-29 11:30:00 -0500

    fi

--- time_display.py
+++ time_display.py
@@ -30,8 +30,7 @@ class TimeDisplay:
         self.colon = pyglet.text.Label(':', font_name='Cat Font', font_size=125, y=320/2,
             color=PINK, anchor_x='center', anchor_y='center')
         self.bannerLabel = pyglet.text.Label('', font_name='Cat Font', font_size=18, x=480/2, 
-            y=320/2+100, color=PINK, anchor_x='center', anchor_y='center', batch=self.batchLabels, 
-            visible=False)
+            y=320/2+100, color=PINK, anchor_x='center', anchor_y='center', batch=self.batchLabels)
         self.update()
 
         pyglet.clock.unschedule(self.clouds.updateSprites)
