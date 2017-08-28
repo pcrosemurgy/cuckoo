91948e722f2d82f6724c85954b8e44ec937c568e
Paul Rosemurgy <pcrosemurgy@gmail.com>
2017-08-28 11:37:54 -0400

    indent

--- time_display.py
+++ time_display.py
@@ -26,7 +26,8 @@ class TimeDisplay:
             y=320/2, color=PINK, anchor_x='center', anchor_y='center', batch=self.batchLabels)
         self.bannerLabel = pyglet.text.Label('', font_name='Cat Font', font_size=18, x=480/2, 
             y=320/2+100, color=PINK, anchor_x='center', anchor_y='center', batch=self.batchLabels)
-        self.colon = pyglet.sprite.Sprite(pyglet.image.load('data/img/colon.png'), anchor_y='center', batch=self.batchLabels)
+        self.colon = pyglet.sprite.Sprite(pyglet.image.load('data/img/colon.png'), batch=self.batchLabels)
+        self.colon.y -= self.colon.height/2.0
         self.update()
 
         pyglet.clock.unschedule(self.clouds.updateSprites)
